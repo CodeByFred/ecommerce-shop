4a6b80df0e60600901b3023d51f40f0b24279b18
FJack <fredlishiajack@gmail.com>
2025-05-11 03:22:21 +1000

    create and style account page

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,4 +1,32 @@
+import classes from "./Account.module.scss";
+import cart from "../../assets/cart.svg";
+import Button from "../../components/Button/Button";
+
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <div className={classes.container}>
+      <form className={classes.form}>
+        <img className={classes.icon} src={cart} alt="cart" />
+        <label htmlFor="usernameInput"></label>
+        <input
+          type="text"
+          id="usernameInput"
+          name="username"
+          placeholder="Username"
+          autoComplete="off"
+        />
+        <label htmlFor="passwordInput"></label>
+        <input
+          type="password"
+          id="passwordInput"
+          name="password"
+          placeholder="Password"
+          autoComplete="off"
+        />
+        <Button variant="action">Login</Button>
+        <small>Forgot Password?</small>
+      </form>
+    </div>
+  );
 };
 export default Account;
